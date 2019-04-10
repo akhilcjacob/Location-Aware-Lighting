5f765d5ebb3dff13916b93f509f674769cebc827
akhil <akhilcjacob@gmail.com>
2019-04-09 23:09:40 -0400

    Finished Gesture nav to control brigtness and preliminary ui

--- WearOS/app/src/main/java/com/lights/locationawarelights/MainActivity.java
+++ WearOS/app/src/main/java/com/lights/locationawarelights/MainActivity.java
@@ -2,20 +2,71 @@ package com.lights.locationawarelights;
 
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
-import android.widget.TextView;
+import android.view.KeyEvent;
+import android.view.View;
+
+import com.github.lzyzsd.circleprogress.ArcProgress;
+
 
 public class MainActivity extends WearableActivity {
+    public static int BRIGHTNESS = 50;
 
-    private TextView mTextView;
+    private ArcProgress arc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mTextView = (TextView) findViewById(R.id.text);
-
-        // Enables Always-on
+        arc = findViewById(R.id.arc);
         setAmbientEnabled();
     }
+
+    @Override /* KeyEvent.Callback */
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        System.out.println("something was pressed");
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_NAVIGATE_NEXT:
+                System.out.println("gesture up");
+                // Do something that advances a user View to the next item in an ordered list.
+                return brightness_up();
+            case KeyEvent.KEYCODE_NAVIGATE_PREVIOUS:
+                System.out.println("gdown");
+                // Do something that advances a user View to the previous item in an ordered list.
+                return brightness_down();
+        }
+        // If you did not handle it, let it be handled by the next possible element as deemed by the Activity.
+        return super.onKeyDown(keyCode, event);
+    }
+
+
+    private int bound(int num) {
+        if (num > 100)
+            return 100;
+        if (num < 0)
+            return 0;
+        return num;
+    }
+
+    private boolean brightness_up() {
+        boolean handled = false;
+        MainActivity.BRIGHTNESS = bound(MainActivity.BRIGHTNESS + 10);
+        arc.setProgress(BRIGHTNESS);
+        return true;
+    }
+
+    private boolean brightness_down() {
+        boolean handled = false;
+        MainActivity.BRIGHTNESS = bound(MainActivity.BRIGHTNESS - 10);
+        arc.setProgress(BRIGHTNESS);
+        return true;
+    }
+
+    public void brightness_up(View view) {
+        brightness_up();
+    }
+    public void brightness_down(View view) {
+        brightness_down();
+    }
 }
+
+
