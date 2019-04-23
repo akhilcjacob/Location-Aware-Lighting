8a85d0aaeb788e71449183114cff1c9c1aa5d377
TLouison <toddmlouison@gmail.com>
2019-04-23 20:26:32 +0100

    Fixed indentation and imports

--- sockets/client.py
+++ sockets/client.py
@@ -1,5 +1,5 @@
 import socket
-from Lights.led import Lights
+from led import Lights
 
 HOST = socket.gethostbyname("rp1")
 PORT = 5210        # The port used by the server
@@ -10,11 +10,11 @@ light = Lights()
 Using the example code from the BlueZ libary to create an BLE advertisement
 '''
 def advertiseBLE():
-	exec(open("~/bluez-5.43/test/example-advertisement").read())
+    exec(open("~/bluez-5.43/test/example-advertisement").read())
 
 if __name__ == "__main__":
     advertise = thread.Thread(target=advertiseBLE)
-	advertise.start()
+    advertise.start()
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((HOST, PORT))
