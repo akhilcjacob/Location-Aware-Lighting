b5c6aa9300bd6f7eadf6e4d6507944d0f2181be5
TLouison <toddmlouison@gmail.com>
2019-04-24 22:50:07 +0100

    added Ally's updated lighting

--- Lights/led.py
+++ Lights/led.py
@@ -14,13 +14,12 @@ class Lights(object):
         self.red.off()
         self.green.off()
         self.blue.off()
-        self.bright = 0
+        self.bright = 80
         self.color = "#000000"
-        self.wait = 0.01
+        self.wait = 0.003
 
     def hexTOrgb(self, hexColor):
         hexColor = hexColor.lstrip('#')
-        
         hlen = len(hexColor)
         return tuple(int(hexColor[i:i+hlen//3], 16) for i in range(0, hlen, hlen//3))
 
@@ -46,14 +45,9 @@ class Lights(object):
         r = colorTo[0]
         g = colorTo[1]
         b = colorTo[2]
-        print(colorTo,colorFrom)
         stepR = self.calculateStep(colorFrom[0], r)
-        print("red step:", stepR)
         stepG = self.calculateStep(colorFrom[1], g)
-        print("grn step:", stepG)
         stepB = self.calculateStep(colorFrom[2], b)
-        print("blu step:", stepB)
-
         redVal = colorFrom[0]
         grnVal = colorFrom[1]
         bluVal = colorFrom[2]
@@ -61,36 +55,43 @@ class Lights(object):
             redVal = self.calculateVal(stepR, redVal, x)
             grnVal = self.calculateVal(stepG, grnVal, x)
             bluVal = self.calculateVal(stepB, bluVal, x)
-
-            pi.set_PWM_dutycycle(self.redPin, redVal)
-            pi.set_PWM_dutycycle(self.greenPin, grnVal)
-            pi.set_PWM_dutycycle(self.bluePin, bluVal)
-
+            pi.set_PWM_dutycycle(self.redPin, redVal*(self.bright/100.0))
+            pi.set_PWM_dutycycle(self.greenPin, grnVal*(self.bright/100.0))
+            pi.set_PWM_dutycycle(self.bluePin, bluVal*(self.bright/100.0))
             time.sleep(self.wait)
 
     def setBrightness(self, targetBrightness):
         rgb = self.hexTOrgb(self.color)
-        redVal = rgb[0]*(targetBrightness/100)
-        grnVal = rgb[1]*(targetBrightness/100)
-        bluVal = rgb[2]*(targetBrightness/100)
-        pi.set_PWM_dutycycle(self.redPin, redVal)
-        pi.set_PWM_dutycycle(self.greenPin, grnVal)
-        pi.set_PWM_dutycycle(self.bluePin, bluVal)
+        while (targetBrightness != self.bright):
+            if (targetBrightness > self.bright) and (targetBrightness-self.bright > 2):
+                self.bright = self.bright + 2
+            elif (targetBrightness < self.bright) and (self.bright-targetBrightness > 2):
+                self.bright = self.bright - 2
+            else:
+                self.bright = targetBrightness
+            if self.bright > 100:
+                self.bright = 100
+            elif self.bright < 0:
+                self.bright = 0
+            redVal = (rgb[0]*(self.bright/100.0))
+            grnVal = (rgb[1]*(self.bright/100.0))
+            bluVal = (rgb[2]*(self.bright/100.0))
+            pi.set_PWM_dutycycle(self.redPin, redVal)
+            pi.set_PWM_dutycycle(self.greenPin, grnVal)
+            pi.set_PWM_dutycycle(self.bluePin, bluVal)
+            time.sleep(0.3)
 
     def setColor(self, hexColor):
         rgb = self.hexTOrgb(hexColor)
-
         self.colorFade(self.hexTOrgb(self.color), rgb)
-        print(self.color)
         self.color = hexColor
 
-        #pi.set_PWM_dutycycle(self.redPin, r)
-        #pi.set_PWM_dutycycle(self.greenPin, g)
-        #pi.set_PWM_dutycycle(self.bluePin, b)
-
-if __name__ == "__main__":
-    l = Lights()
-    while True:
-        l.setColor('#0000ff')
-        time.sleep(2)
-        l.setColor('#ff0000')
+# l = Lights()
+# while True:
+#     l.setColor('#ff0000')
+#     l.setBrightness(20)
+#     l.setColor('#0000ff')
+#     l.setBrightness(0)
+#     l.setColor('#00ff00')
+#     l.setBrightness(50)
+# time.sleep(3)
